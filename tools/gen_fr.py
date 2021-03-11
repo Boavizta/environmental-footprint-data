17aa75f10f17eda5e5b053d5a1d16a5bc96f92c9
AirLoren <romain@cmakers.io>
2021-03-11 10:38:24 +0100

    Update US to FR script

--- tools/gen_fr.py
+++ tools/gen_fr.py
@@ -1,33 +1,21 @@
 import csv
 from datetime import datetime
 import locale
+import re
+import argparse
 
-output = open('boavizta-data-fr.csv', 'w')
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-s", "--source", required=True, help="CSV file to be converted")
+argparser.add_argument("-o", "--output", required=True, help="output CSV file")
+args = vars(argparser.parse_args())
+source = args["source"]
+output = args["output"]
+
+output = open(output, 'w')
 writer = csv.writer(output, delimiter=';')
-with open('boavizta-data-us.csv') as csvfile:
+with open(source) as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        # Convert number format
-        row[5] = row[5].replace('.', ',')
-        row[6] = row[6].replace('.', ',')
-        row[7] = row[7].replace('.', ',')
-        row[11] = row[11].replace('.', ',')
-        row[12] = row[12].replace('.', ',')
-        row[13] = row[13].replace('.', ',')
-
-        # Convert date
-        if row[9].count(' ') == 1:
-            locale.setlocale(locale.LC_TIME, "en_US.utf8")
-            d = datetime.strptime(row[9], '%B %Y')
-            locale.setlocale(locale.LC_TIME, "fr_FR.utf8")
-            row[9] = d.strftime('%B %Y').capitalize()
-        elif row[9].count(' ') == 2:
-            locale.setlocale(locale.LC_TIME, "en_US.utf8")
-            d = datetime.strptime(row[9], '%B %d, %Y')
-            locale.setlocale(locale.LC_TIME, "fr_FR.utf8")
-            row[9] = d.strftime('%-d %B %Y')
-
-        # Save
-        writer.writerow(row)
-
+        updatedrow = [re.sub(r'(.*[0-9])\.([0-9].*)', r'\1,\2', content) for content in row]
+        writer.writerow(updatedrow)
 output.close()
