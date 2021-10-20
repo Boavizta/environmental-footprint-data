7914ffb8c0cb7583429d85debbefd3411513a98e
bpetit <bpetit@hubblo.org>
2021-10-20 20:04:51 +0200

    Adding scope 2 vs scope 3 statistics to frontend (#32)

--- frontend/index.js
+++ frontend/index.js
@@ -159,4 +159,148 @@ function gridit(csv) {
 
   const eGridDiv = document.querySelector('#myGrid');
   new agGrid.Grid(eGridDiv, gridOptions);
+
+  // Manufacturing % Use % Use Location . Total (kgCO2eq) .
+    // use vs manuf global
+    //  use vs manuf by use location
+    // use vs manuf by use location & assembly & build location
+    // use vs manuf by vendor
+
+  console.log(objectsData);
+
+  let avg_use = 0;
+  let avg_manuf = 0;
+  let avg_lifetime = 0;
+  let use_fails = 0;
+  let manuf_fails = 0;
+  let nb_lifetimes = 0;
+  let per_use_loc = Object();
+  for (let i = 0; i < objectsData.length; i++) {
+    if ( objectsData.indexOf("Use Location") && ! objectsData[i]["Use Location"] == "" ) {
+      if ( ! (objectsData[i]["Use Location"] in per_use_loc) ) {
+        per_use_loc[objectsData[i]["Use Location"]] = {
+            avg_use: 0,
+            avg_manuf: 0,
+            nb_use: 0,
+            nb_manuf: 0
+        };
+      }
+    }
+    if (! objectsData[i]["Use (%)"] == "") {
+      let res_use = parseFloat(objectsData[i]["Use (%)"].replace('%', ''));
+      let res_lifetime = objectsData[i]["Lifetime"];
+      if (! isNaN(res_use)) {
+        avg_use = avg_use + res_use;
+        if ( res_lifetime != "" ) {
+            console.log("avg_lifetime="+avg_lifetime+" res_lifetime="+res_lifetime);
+            avg_lifetime += parseFloat(res_lifetime);
+            nb_lifetimes++;
+        }
+        if ( objectsData[i]["Use Location"] in per_use_loc ) {
+          per_use_loc[objectsData[i]["Use Location"]]["avg_use"] = per_use_loc[objectsData[i]["Use Location"]]["avg_use"] + res_use;
+          per_use_loc[objectsData[i]["Use Location"]]["nb_use"]++;
+        }
+      } else {
+        use_fails++;
+      }
+    } else {
+      use_fails++;
+    }
+    if (! objectsData[i]["Manufacturing"] == "") {
+      let res_manuf = parseFloat(objectsData[i]["Manufacturing"].replace('%', ''));
+      if (! isNaN(res_manuf)) {
+        avg_manuf = avg_manuf + res_manuf;
+        if ( objectsData[i]["Use Location"] in per_use_loc ) {
+
+          per_use_loc[objectsData[i]["Use Location"]]["avg_manuf"] = per_use_loc[objectsData[i]["Use Location"]]["avg_manuf"] + res_manuf;
+          per_use_loc[objectsData[i]["Use Location"]]["nb_manuf"]++;
+        }
+      } else {
+        manuf_fails++;
+      }
+    } else {
+      manuf_fails++;
+    }
+  }
+  avg_use = avg_use / (parseFloat(objectsData.length) - use_fails);
+  avg_manuf = avg_manuf / (parseFloat(objectsData.length) - manuf_fails);
+  var pieOptions = {
+  	segmentShowStroke: false,
+  	animateScale: true
+  }
+  var pie_elmt = document.getElementById("camembert");
+  var caption = document.createElement("P");
+  pie_elmt.parentNode.appendChild(caption);
+  var camembert = pie_elmt.getContext("2d");
+  //var camemberts = new Array();
+  var pieData =
+        [
+          {
+            value: avg_use,
+  		    color: "#9277a8",
+            label: "Use phase"
+  	      },
+  	      {
+     		value: avg_manuf,
+     		color: "#357dcc",
+            label: "Manufacturing"
+     	  },
+          {
+            value: (100 - avg_manuf - avg_use),
+            color: "#2596be",
+            label: "Rest of the lifecycle"
+          }
+        ];
+
+  var positioner = "right";
+  for (const u in per_use_loc) {
+    if ( u != "WW") {
+        let use = (parseFloat(per_use_loc[u]["avg_use"])/parseFloat(per_use_loc[u]["nb_use"]));
+        let manuf = (parseFloat(per_use_loc[u]["avg_manuf"])/parseFloat(per_use_loc[u]["nb_manuf"]));
+        let rest = 100 - use - manuf;
+        if (isNaN(manuf) && !isNaN(use)) {
+          rest = 100 - use;
+          manuf = 0;
+        }
+        var new_camembert = document.getElementById("camembert").cloneNode(true);
+        var new_camembert_ctx = new_camembert.getContext("2d");
+        new_camembert.id = "loc-"+u;
+        var title = document.createElement("H4");
+        title.innerText = "Ratio scope 2/scope 3 par lieu d'utilisation : "+u;
+        var new_div = document.createElement("DIV");
+        new_div.className = "camembert-"+positioner;
+        new_div.appendChild(title);
+        new_div.appendChild(new_camembert);
+        document.getElementById("stats").appendChild(new_div);
+
+        var newPieData =
+            [
+              {
+                value: use,
+  		        color: "#9277a8",
+                label: "Use phase"
+  	          },
+  	          {
+         		value: manuf,
+     		    color: "#357dcc",
+                label: "Manufacturing"
+         	  },
+              {
+                value: rest,
+                color: "#2596be",
+                label: "Rest of the lifecycle"
+              }
+            ];
+
+
+        new Chart(new_camembert_ctx).Pie(newPieData, pieOptions);
+        if (positioner == "left") {
+          positioner = "right";
+        } else {
+          positioner = "left";
+        }
+    }
+  }
+  caption.innerText = "Average lifetime : "+(avg_lifetime/nb_lifetimes);
+  new Chart(camembert).Pie(pieData, pieOptions);
 };
