3947b0680e57c2f46796ad8b9793912dd5332e11
da-ekchajzer <david_ekchajzer@Laptop.dek>
2022-03-18 16:49:39 +0100

    add scope 2 & 3 impacts

--- data-explorer/src/lib/DataExplorer.svelte
+++ data-explorer/src/lib/DataExplorer.svelte
@@ -10,6 +10,31 @@
     let lifetime;
     let region;
     let disabledSearchButton;
+    let rows_selection;
+
+    function impactScope3():number{
+        let scope3 = 0;
+        rows_selection.forEach(row => scope3 += row["Total (kgCO2eq)"]*row["Manufacturing"]);
+        return scope3
+    }
+
+    function electricalImpactFactor(location: Location):number {
+        return 0;
+    }
+
+    function impactScope2():number{
+        let scope2 = 0;
+        if(lifetime == undefined && region == undefined){
+            rows_selection.forEach(row => scope2 += row["Total (kgCO2eq)"]*row["Use"]);
+        }else if (lifetime != undefined && region == undefined){
+            rows_selection.forEach(row => scope2 += ((row["Total (kgCO2eq)"]*row["Use"])/row["Lifetime"])*lifetime);
+        }else if (lifetime == undefined && region != undefined){
+            rows_selection.forEach(row => scope2 += row["Yearly TEC (kWh)"]*row["Lifetime"]*electricalImpactFactor(location));
+        }else if (lifetime != undefined && region != undefined){
+            rows_selection.forEach(row => scope2 += row["Yearly TEC (kWh)"]*lifetime*electricalImpactFactor(location));
+        }
+        return scope2
+    }
 
 </script>
 <div>
