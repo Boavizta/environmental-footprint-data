4ad5f3ef72bfa404186f5ef0352e841bcb946022
Bruno Thomas <bruno@barreverte.fr>
2022-03-18 11:49:34 +0100

    first svelte scafold

---
+++ data-explorer/src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let name = "Boavizta"
+</script>
+
+<h1>Welcome to {name}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
