ee64a14f66f63bbe1b5c04c1cc63a3ac282ac475
Bruno Thomas <bruno@barreverte.fr>
2022-03-18 16:32:12 +0100

    fix: AgGrid compilation error ?

--- data-explorer/svelte.config.js
+++ data-explorer/svelte.config.js
@@ -4,13 +4,14 @@ import * as path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter(),
 		vite: {
+			optimizeDeps: {
+				include: ['@budibase/svelte-ag-grid']
+			},
 			resolve: {
 				alias: {
 					$: path.resolve('./src/')
