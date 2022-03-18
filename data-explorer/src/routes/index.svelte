01975f5765657b4381e4f7a4743f4f92a8a99dae
Bruno Thomas <bruno@barreverte.fr>
2022-03-18 12:02:09 +0100

    feat: add ag grid component

--- data-explorer/src/routes/index.svelte
+++ data-explorer/src/routes/index.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
-	export let name = "Boavizta"
+	import AgGrid from "@budibase/svelte-ag-grid";
+
+	let data = [
+		{ make: "Toyota", model: "Celica", price: 35000 },
+		{ make: "Ford", model: "Mondeo", price: 32000 },
+		{ make: "Porsche", model: "Boxter", price: 72000 },
+	];
+
+	let columnDefs = [
+		{
+			headerName: "Make",
+			field: "make",
+			sortable: true,
+			editable: true,
+		},
+		{ headerName: "Model", field: "model", sortable: true },
+		{ headerName: "Price", field: "price", sortable: true },
+	];
 </script>
 
-<h1>Welcome to {name}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    :global(:root) {
+        --grid-height: 500px;
+    }
+</style>
+
+<AgGrid bind:data {columnDefs} />
