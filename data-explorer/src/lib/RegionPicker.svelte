b245c9d34a0ec653320db4d05aa4cce39b087b6f
da-ekchajzer <david_ekchajzer@Laptop.dek>
2022-03-22 15:05:07 +0100

    regio picker structure

--- data-explorer/src/lib/RegionPicker.svelte
+++ data-explorer/src/lib/RegionPicker.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
+    import  {Select} from "svelte-select"
     let region = "eu"
+
+    onMount(async () => {
+        const res = await fetch("./electrical_foot_print.csv");
+        const text = await res.text();
+    });
+
+
+    let items = [
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'pizza', label: 'Pizza'},
+        {value: 'cake', label: 'Cake'},
+        {value: 'chips', label: 'Chips'},
+        {value: 'ice-cream', label: 'Ice Cream'},
+    ];
+
+    let value = {value: 'cake', label: 'Cake'};
+
+    function handleSelect(event) {
+    }
 </script>
 
-<input value="{region}" name="region" type="text">
+<Select {items} {value} on:select={handleSelect}></Select>
