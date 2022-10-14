597931b5786b43f2e8b4ac40c573b6ad928be45f
Pablo Seminario <pablo@seminar.io>
2022-10-14 14:50:57 +0200

    Add Apple parser

--- tools/spiders/apple.py
+++ tools/spiders/apple.py
@@ -1,12 +1,10 @@
 """Spider to explore Apple Carbon footprints.
 
 This spider:
- - scrape the landing page, then
- - extract all links to tabs like "Desktops", "Notebooks", etc.
- - scrape the corresponding links which list devices for each category
+ - scrape the environment page, then
+ - extract all links to PDFs
  - extract all links to Carbon Footprint PDFs
- - scrape the corresponding links (except if they were already in existing sources).
- - use the dell parser to extract info.
+ - use the apple parser to extract info.
 
 Note that extracting the whole info is quite long, so be patient.
 """
@@ -19,7 +17,7 @@ from os import link
 from typing import Any, Iterator
 
 from tools.spiders.lib import spider
-#from tools.parsers import apple
+from tools.parsers import apple
 from tools.parsers.lib import data
 
 import scrapy
@@ -45,11 +43,8 @@ class AppleSpider(spider.BoaViztaSpider):
     def parse_carbon_footprint(
         self, response: http.Response, **unused_kwargs: Any,
     ) -> Iterator[Any]:
-        """Parse a Google Product Carbon footprint document."""
-        """
+        """Parse a Apple Product Carbon footprint document."""
         for device in apple.parse(io.BytesIO(response.body), response.url):
             device.data['sources'] = response.url
             device.data['sources_hash']=data.md5(io.BytesIO(response.body))
             yield device.reorder().data
-        """
-        return
